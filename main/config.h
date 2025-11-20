802d8c69946eb2f8acf920ce0f570ec5a026beef
Andres Alvarez <133549200+andres-alvarez19@users.noreply.github.com>
2025-11-20 03:57:34 -0300

    Change WiFi and ThingsBoard credentials
    Updated WiFi credentials and ThingsBoard access token.

--- main/config.h
+++ main/config.h
@@ -1,8 +1,8 @@
 #pragma once
 
 // Credenciales WiFi
-#define WIFI_SSID     "FAMILIA MORALES"
-#define WIFI_PASS     "alvarezmorales"
+#define WIFI_SSID     "SSID"
+#define WIFI_PASS     "password"
 
 // Identificador de dispositivo (para logs y clientId)
 #define DEVICE_LABEL  "esp_wroom_32"
@@ -10,7 +10,7 @@
 // Configuracion de ThingsBoard
 #define TB_HOST         "iot.ceisufro.cl"
 #define TB_PORT         1883
-#define TB_ACCESS_TOKEN "TrPoEAdBd7PNI46BLPC5"
+#define TB_ACCESS_TOKEN "TOKEN"
 
 // Habilitar/deshabilitar buzzer si no está conectado
 #define BUZZER_ENABLED 1
